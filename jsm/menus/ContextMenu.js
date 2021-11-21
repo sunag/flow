2ad070b910677fabd32ca9c64d949d481dcbdceb
sunag <jcdeconto@igalia.com>
2021-11-21 19:42:23 -0300

    hide last context if other is opened

--- jsm/menus/ContextMenu.js
+++ jsm/menus/ContextMenu.js
@@ -1,10 +1,38 @@
 import { Menu } from './Menu.js';
 
+let lastContext = null;
+
 export class ContextMenu extends Menu {
 
 	constructor( target = null ) {
 
 		super( 'context', target );
+
+	}
+
+	show( x, y ) {
+
+		if ( lastContext !== null ) {
+
+			lastContext.hide();
+
+		}
+
+		lastContext = this;
+
+		return super.show( x, y );
+
+	}
+
+	hide() {
+
+		if ( lastContext === this ) {
+
+			lastContext = null;
+
+		}
+
+		return super.hide();
 
 	}
 
